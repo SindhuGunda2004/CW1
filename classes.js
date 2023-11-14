1c229797c5c842af1b02745995b7e02326984466
SindhuGunda24 <sindhugunda1592@gmail.com>
2023-11-14 11:47:32 +0400

    JS file will all subjects

---
+++ classes.js
@@ -0,0 +1,82 @@
+let classes = [
+    {
+        "id": 1001, 
+        "subject": "Badminton",
+        "price": 400, 
+        "image": "./images/badminton.png",
+        "slotsAvailable": 5,
+        "location": "Nadd Al Sheba"
+    },
+    {
+        "id": 1002, 
+        "subject": "Table Tennis",
+        "price": 300, 
+        "image": "./images/TT.png",
+        "slotsAvailable": 5,
+        "location": "Barsha"
+    },
+    {
+        "id": 1003, 
+        "subject": "Swimmming",
+        "price": 450, 
+        "image": "./images/swim.png",
+        "slotsAvailable": 5,
+        "location": "Bur Dubai"
+    },
+    {
+        "id": 1004, 
+        "subject": "Squash",
+        "price": 350, 
+        "image": "./images/squash.png",
+        "slotsAvailable": 5,
+        "location": "Al Khail"
+    },
+    {
+        "id": 1005, 
+        "subject": "Surfing",
+        "price": 550, 
+        "image": "./images/surfing.png",
+        "slotsAvailable": 5,
+        "location": "Jumeirah"
+    }, 
+    {
+        "id": 1006, 
+        "subject": "Basketball",
+        "price": 375, 
+        "image": "./images/basketball.png",
+        "slotsAvailable": 5,
+        "location": "Nadd al Hamar"
+    }, 
+    {
+        "id": 1007, 
+        "subject": "Throw Ball",
+        "price": 200, 
+        "image": "./images/throwball.png",
+        "slotsAvailable": 5,
+        "location": "Al Nahda"
+    }, 
+    {
+        "id": 1008, 
+        "subject": "Football",
+        "price": 450, 
+        "image": "./images/football.png",
+        "slotsAvailable": 5,
+        "location": "Media City"
+    }, 
+    {
+        "id": 1009, 
+        "subject": "Cricket",
+        "price": 350, 
+        "image": "./images/cricket.png",
+        "slotsAvailable": 5,
+        "location": "Karama"
+    }, 
+    {
+        "id": 1010, 
+        "subject": "Rugby",
+        "price": 275, 
+        "image": "./images/rugby.png",
+        "slotsAvailable": 5,
+        "location": "Jumeirah"
+    }
+]
